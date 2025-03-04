d8b6da20a470c078aa22e0112162e4116ac46c09
bahetiabhishek <baheti.abhishek@gmail.com>
2025-03-04 17:33:30 +0100

    second commit

---
+++ src/main/java/com/daimlertruck/sample/DemoSpringAzureAppApplication.java
@@ -0,0 +1,22 @@
+package com.daimlertruck.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoSpringAzureAppApplication {
+	
+	@GetMapping("/greet")
+	public String greetMe()
+	{
+		return "Hello Abhishek";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringAzureAppApplication.class, args);
+	}
+
+}
